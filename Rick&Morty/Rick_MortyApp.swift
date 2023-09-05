711960ad4dbaee7fe79c75131419ed94484f5e29
David Diego Gomez <diego@MBP-de-David.fibertel.com.ar>
2023-09-05 13:04:26 -0300

    first commit

---
+++ Rick&Morty/Rick_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyApp.swift
+//  Rick&Morty
+//
+//  Created by David Diego Gomez on 05/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
